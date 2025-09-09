35e8a7b6964c3bbd3d39d4c81a36d09977afe7ac
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-09-09 10:46:04 +0530

    Add initial Java practice files
    Added three new Java files: Day2_09_Sep_01.java demonstrating data types, Employee.java for employee info output, and First_09_Sep.java with a welcome message. These files serve as basic practice examples.

---
+++ Practices/Day2_09_Sep_01.java
@@ -0,0 +1,42 @@
+class Day2_09_Sep_01{
+	
+	public static void main(String args[]){
+		/* byte by ;
+		by = 127;
+		System.out.println(by);
+		
+		long lg ;
+		lg = 100000000000000000L;
+		System.out.println(lg);
+		
+		float fl = 10.5f;
+		System.out.println(fl);
+		
+		double db = 10.567d;
+		System.out.println(db);
+		
+		char ch = 'A';
+		System.out.println(ch);
+		
+		char ch1 = 126;
+		System.out.println(ch1);
+		
+		char ch4 = 0b1000001;
+		System.out.println(ch4);
+		
+		char ch5 = '\u0041';
+		System.out.println(ch5); */
+		
+		char ch6 = 92;
+		System.out.println(ch6);
+		
+		
+		int in1 = '#';
+		System.out.println(in1);
+		
+		
+		
+		
+		
+	}
+}
---
+++ Practices/Employee.java
@@ -0,0 +1,14 @@
+class Employee{
+	
+	public static void main(String args[]){
+		System.out.print("****************************** \n Employee Info \n ********************************* \n Employee Id : 10 \n Employee Name: John \n Employee Basic Salary : 35000 \n" );
+		
+		System.out.print("************************************ \n");
+		System.out.print("Employee Info \n");
+		System.out.print("************************************ \n");
+		System.out.print("Employee Id :\t \'1000\' \n");
+		System.out.print("Employee name :\t John \n");
+		System.out.print("Employee Basic Salary :\t35000 \n");
+		System.out.print("***********************************");
+	};
+};
