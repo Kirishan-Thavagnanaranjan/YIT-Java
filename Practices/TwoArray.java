0e746e41168b48bfcd4d98e9a5f1b10196a3d045
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-09-15 20:23:41 +0530

    Add TwoArray example for 2D array usage
    Introduces TwoArray.java, demonstrating initialization, assignment, and access of elements in a two-dimensional int array.

---
+++ Practices/TwoArray.java
@@ -0,0 +1,31 @@
+class TwoArray{
+	
+	public static void main(String args[]){
+		
+		int[][] x = new int[5][2];
+		
+		x[0][0] = 45;
+		x[0][1] = 50;
+		
+		x[1][0] = 43;
+		x[1][1] = 35;
+		
+		x[2][0] = 56;
+		x[2][1] = 67;
+		
+		x[3][0] = 56;
+		x[3][1] = 50;
+	
+		x[4][0] = 46;
+		x[4][1] = 88;
+		
+		System.out.println(x[0][1]);
+		System.out.println(x[1][0]);
+		System.out.println(x[2][0]);
+		System.out.println(x[3][1]);
+		System.out.println(x[4][0]);
+
+	}
+}
+		
+		
