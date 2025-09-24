65f63cc0fdca4f311010e07c656b1b5fb2813366
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-09-24 18:49:12 +0530

    Update Student instantiation with constructor arguments
    Changed the creation of the Student object to use a constructor with ID and name parameters instead of the default constructor.

--- Practices/OOP/Day4_22_09_2025/MainStudent.java
+++ Practices/OOP/Day4_22_09_2025/MainStudent.java
@@ -2,7 +2,7 @@ class MainStudent{
 	
 	public static void main(String args[]){
 		
-		Student stu1 = new Student();
+		Student stu1 = new Student(01,"Kirishan");
 		stu1.setMarks(80,90,95);
 		stu1.Student("Kirish",2000);
 		stu1.display();
